27ec5ecb8a1724fac0a47baaa5977f76f5fe6aa4
kenny40100 <71364976+kenny40100@users.noreply.github.com>
2020-10-17 14:42:13 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,34 @@
 let y = 4
 let x = 4
-let z = 0
-let w = 4
+for (let index = 0; index < 5; index++) {
+    led.plot(x, y)
+    basic.pause(100)
+    x = x - 1
+}
+y = y - 1
+for (let index = 0; index < 6; index++) {
+    led.plot(x, y)
+    basic.pause(100)
+    x = x + 1
+}
+y = y - 1
+for (let index = 0; index < 6; index++) {
+    led.plot(x, y)
+    basic.pause(100)
+    x = x - 1
+}
+y = y - 1
+for (let index = 0; index < 6; index++) {
+    led.plot(x, y)
+    basic.pause(100)
+    x = x + 1
+}
+y = y - 1
+for (let index = 0; index < 6; index++) {
+    led.plot(x, y)
+    basic.pause(100)
+    x = x - 1
+}
 basic.forever(function () {
-    if (x >= z) {
-        for (let index = 0; index < 5; index++) {
-            led.plot(x, y)
-            basic.pause(100)
-            x = x - 1
-        }
-    } else if (x <= w) {
-        y = y - 1
-        for (let index = 0; index < 5; index++) {
-            led.plot(x, y)
-            basic.pause(100)
-            x = x + 1
-        }
-    }
+	
 })
