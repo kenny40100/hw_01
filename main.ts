d34377ba3429b07b31af44bc6d9d1da1b4f25f7b
kenny40100 <71364976+kenny40100@users.noreply.github.com>
2020-10-20 15:53:10 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,16 @@
-let y = 4
 let x = 4
-for (let index = 0; index < 5; index++) {
-    led.plot(x, y)
-    basic.pause(100)
-    x = x - 1
-}
-y = y - 1
-for (let index = 0; index < 6; index++) {
-    led.plot(x, y)
-    basic.pause(100)
-    x = x + 1
-}
-y = y - 1
-for (let index = 0; index < 6; index++) {
-    led.plot(x, y)
-    basic.pause(100)
-    x = x - 1
-}
-y = y - 1
-for (let index = 0; index < 6; index++) {
-    led.plot(x, y)
-    basic.pause(100)
-    x = x + 1
-}
-y = y - 1
-for (let index = 0; index < 6; index++) {
-    led.plot(x, y)
-    basic.pause(100)
-    x = x - 1
+let y = 4
+let direction = -1
+while (y >= 0) {
+    while (x >= 0 && x <= 4) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(x, y)
+            basic.pause(100)
+            x += -1
+        }
+    }
+    y += -1
+    x = x * direction
 }
 basic.forever(function () {
 	
